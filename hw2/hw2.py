f295ebe9046f6c1d63d364e99b75702b7fd25cbb
Miranda Myers <mirandacmyers12@gmail.com>
2014-09-14 17:59:22 -0700

    Step 3 finished

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -39,10 +39,10 @@ def create_frequency_diagram(table, index, title, xlabel, ylabel, outfile):
     xs = get_column_as_floats(table, index)
     values, counts = calculate_frequencies(xs)
             
-    #Reset the figure
+    #Resets the figure
     pyplot.figure()
     
-    #Generate histogram
+    #Generates frequency diagram
     pyplot.bar(values, counts, align='center')
     pyplot.grid(True)
     pyplot.title(title)
@@ -56,15 +56,15 @@ def create_all_freq_diagrams():
     """Creates frequency diagrams of all categorical attributes of auto-data.txt dataset."""
     table = read_csv('auto-data.txt')
     
-    #Create cylinder diagram
+    #Creates cylinder diagram
     create_frequency_diagram(table, 1, 'Total Number of Cars by Number of Cylinders', \
         'Cylinders', 'Count', 'step-1-cylinders.pdf')
     
-    #Create model year diagram
+    #Creates model year diagram
     create_frequency_diagram(table, 6, 'Total Number of Cars by Year', 'Year', 'Count', \
         'step-1-modelyear.pdf')
     
-    #Create origin diagram
+    #Creates origin diagram
     create_frequency_diagram(table, 7, 'Total Number of Cars by Origin', 'Origin', \
         'Count', 'step-1-origin.pdf')
         
@@ -83,25 +83,62 @@ def create_pie_chart(table, index, title, outfile):
     
     pyplot.savefig(outfile)
     
-    
 def create_all_pie_charts():
     """Creates pie charts of all categorical attributes of auto-data.txt dataset."""
     table = read_csv('auto-data.txt')
     
-    #Create cylinder pie chart
+    #Creates cylinder pie chart
     create_pie_chart(table, 1, 'Total Number of Cars by Number of Cylinders\n', 'step-2-cylinders.pdf')
     
-    #Create model year pie chart
+    #Creates model year pie chart
     create_pie_chart(table, 6, 'Total Number of Cars by Year\n', 'step-2-modelyear.pdf')
     
-    #Create origin pie chart
+    #Creates origin pie chart
     create_pie_chart(table, 7, 'Total Number of Cars by Origin', 'step-2-origin.pdf')
+
+def create_dot_chart(table, index, title, xlabel, outfile):
+    xs = get_column_as_floats(table, index)
+    values, counts = calculate_frequencies(xs)
+     
+    #Initializes all ys to 1
+    ys = [1 for item in xs]  
+
+    pyplot.figure()
+    pyplot.title(title)
+    pyplot.xlabel(xlabel)
+    pyplot.plot(xs, ys, alpha=0.2, marker='.', markersize=15, linestyle='None')
+    pyplot.gca().get_yaxis().set_visible(False)
+    pyplot.savefig(outfile)
+    
+
+#mpg, cylinders, displacement, horsepower, weight, acceleration, model year, origin, and car name
+def create_all_dot_charts():
+    table = read_csv('auto-data.txt')
+    
+    #Creates mpg dot chart
+    create_dot_chart(table, 0, 'Miles Per Gallon of All Cars', 'MPG', 'step-3-mpg.pdf')
+    
+    #Creates displacement dot chart
+    create_dot_chart(table, 2, 'Displacement of All Cars', 'Displacement', 'step-3-displacement.pdf')
+    
+    #Creates horesepower dot chart
+    create_dot_chart(table, 3, 'Horsepower of All Cars', 'Horsepower', 'step-3-horsepower.pdf')
+    
+    #Creates weight dot chart
+    create_dot_chart(table, 4, 'Weight of All Cars', 'Weight', 'step-3-weight.pdf')
+    
+    #Creates acceleration dot chart
+    create_dot_chart(table, 5, 'Acceleration of All Cars', 'Acceleration', 'step-3-acceleration.pdf')
+
+    #Creates msrp dot chart
+    create_dot_chart(table, 9, 'MSRP of All Cars', 'MSRP', 'step-3-msrp.pdf')
 
 
 
 def main():
     create_all_freq_diagrams()
     create_all_pie_charts()
+    create_all_dot_charts()
 
 
 main()
