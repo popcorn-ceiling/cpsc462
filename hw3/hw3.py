088b49537fef270911a48a64dd2ec810f551669a
Miranda Myers <mirandacmyers12@gmail.com>
2014-10-04 14:41:41 -0700

    Step 1 complete

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 """hw3.py:  Data mining assignment #3: Data classification."""
 
 __author__ = "Dan Collins and Miranda Myers"
@@ -7,6 +8,7 @@ __author__ = "Dan Collins and Miranda Myers"
 import numpy 
 import csv
 import operator
+import random
 
 class DataClassification:
     """FIXME."""
@@ -40,35 +42,7 @@ class DataClassification:
         else:
             return 0
 
-    def discretize_mpg_DoE(self, index):
-        """Converts mpg into a categorical attribute using US \
-           Department of Energy ratings."""
-        xs = self.get_column_as_floats(self.__table, index)
-        counts = [0 for i in range(10)]
-        ratings = [i+1 for i in range(10)]
     
-        #Creates frequency counts based on the ratings
-        for i in range(len(xs)):
-            if xs[i] <= 13:
-                counts[0] += 1
-            if xs[i] == 14:
-                counts[1] += 1
-            if xs[i] >= 15 and xs[i] <= 16:
-                counts[2] += 1
-            if xs[i] >= 17 and xs[i] <= 19:
-                counts[3] += 1
-            if xs[i] >= 20 and xs[i] <= 23:
-                counts[4] += 1
-            if xs[i] >= 24 and xs[i] <= 26:
-                counts[5] += 1
-            if xs[i] >= 27 and xs[i] <= 30:
-                counts[6] += 1
-            if xs[i] >= 31 and xs[i] <= 36:
-                counts[7] += 1
-            if xs[i] >= 37 and xs[i] <= 44:
-                counts[8] += 1
-            if xs[i] >= 45:
-                counts[9] += 1
 
     def calculate_least_squares_lr(self, xs, ys):
         """Calculates the slope (m) and y-intercept (b) of the linear \
@@ -107,9 +81,88 @@ class DataClassification:
         stdy = numpy.std(ys)
     
         return float(cov/(stdx*stdy))
+        
+        
+    def classify_mpg_lr(self, table, index, x):
+    
+        #Get the list of values to be compared with mpg (weight for this program)
+        xs = self.get_column_as_floats(table, index)
+        
+        #Get the list of mpg values
+        ys = self.get_column_as_floats(table, 0)
+        
+        #Calculate linear regression values for mpg and given variable (weight)
+        m, b = self.calculate_least_squares_lr(xs, ys)
+        
+        #Calculate the predicted value for the given x (a weight value)
+        y = m * x + b
+        
+        #Classify based on department of energy ratings
+        classification = self.classify_mpg_DoE(y)
+        
+        return classification
+        
+        
+    def classify_mpg_DoE(self, y): 
+                   
+        if y < 14:
+            rating = 1
+        elif y == 14:
+            rating = 2
+        elif y > 14 and y <= 16:
+            rating = 3
+        elif y > 16 and y <= 19:
+            rating = 4
+        elif y > 19 and y <= 23:
+            rating = 5
+        elif y > 23 and y <= 26:
+            rating = 6
+        elif y > 26 and y <= 30:
+            rating = 7
+        elif y > 30 and y <= 36:
+            rating = 8
+        elif y > 36 and y <= 44:
+            rating = 9
+        elif y > 44:
+            rating = 10
+        #Implement error checking else statement
+        #Ask bowers about edges of bins
+                
+        return rating
+        
+    #def print_mpg_classification(self):
+        
+
+    def test_random_instances(self):
+    
+        print '==========================================='
+        print 'STEP 1: Linear Regression MPG Classifier'
+        print '==========================================='
+        
+        weights = self.get_column_as_floats(self.__table, 4)
+                    
+        testValues = []
+        printIndices = []
+        for i in range(5):
+            rand_i = random.randint(0, len(weights))
+            val = weights[rand_i]
+            
+            printIndices.append(rand_i)
+            testValues.append(val)
+                        
+        for i in range(len(testValues)):
+            classification = self.classify_mpg_lr(self.__table, 4, testValues[i])
+            instance = self.__table[printIndices[i]]
+            actual = float(instance[0])
+            actual = self.classify_mpg_DoE(actual)
+            print '    instance: ', instance
+            print '    class: ', classification, 'actual: ', actual        
+     
     
 def main():
-    """FIXME."""
+    d = DataClassification()
+    
+    d.test_random_instances()
 
 
 if __name__ == "__main__":
